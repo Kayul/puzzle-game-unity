302a2e355e206e6ac25a0b8325b7238b484e14c8
dframe1997 <43210899+dframe1997@users.noreply.github.com>
2020-11-21 15:53:39 +0000

    Update PlayerMovement.cs
    Added basic player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    float playerLocX;
+    float playerLocY;
+    float playerLocZ;
+    public float moveSpeed = 0.01f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerLocX = gameObject.transform.position.x;
+        playerLocY = gameObject.transform.position.y;
+        playerLocZ = gameObject.transform.position.z;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        playerLocY = gameObject.transform.position.y;
+        if(Input.GetKey("w"))
+        {
+            playerLocZ += moveSpeed;
+            gameObject.transform.position = new Vector3(playerLocX, playerLocY, playerLocZ);
+        }
+        if (Input.GetKey("s"))
+        {
+            playerLocZ -= moveSpeed;
+            gameObject.transform.position = new Vector3(playerLocX, playerLocY, playerLocZ);
+        }
+        if (Input.GetKey("a"))
+        {
+            playerLocX -= moveSpeed;
+            gameObject.transform.position = new Vector3(playerLocX, playerLocY, playerLocZ);
+        }
+        if (Input.GetKey("d"))
+        {
+            playerLocX += moveSpeed;
+            gameObject.transform.position = new Vector3(playerLocX, playerLocY, playerLocZ);
+        }
     }
 }
